9c8755ec9717f0133d7531b5d474cdf9dda062d8
ansnekit <ansnekit@gmail.com>
2022-07-27 01:09:00 +0300

    learn router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const app = express();
 const port = 7000;
@@ -6,6 +7,8 @@ const port = 7000;
 app.get('/hello', (_, res) => {
     res.send('Здорова');
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`Сервер запущен. Прослушиваю порт ${port}`);
